34c2b1008919bfc0e5bf0477c1985a4b3a9a91e2
getssh <gtamrat33@gmail.com>
2023-05-12 02:31:53 +0300

    Add fourth transaction from instruction

--- queries.sql
+++ queries.sql
@@ -33,3 +33,13 @@ DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+/* Forth transaction */
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO my_savepoint;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
