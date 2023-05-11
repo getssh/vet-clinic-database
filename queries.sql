2c4ed06a2b2e3024463ed2cb74bc2bc1c4014cfe
getssh <gtamrat33@gmail.com>
2023-05-12 02:14:19 +0300

    Add second transaction from instruction

--- queries.sql
+++ queries.sql
@@ -16,3 +16,12 @@ UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+/* Second transaction */
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
