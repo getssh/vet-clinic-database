7a5d1438e8103ca6719fbba784923140649b27aa
getssh <gtamrat33@gmail.com>
2023-05-13 02:18:13 +0300

    Add colums species id and owner id with forign key

--- schema.sql
+++ schema.sql
@@ -27,3 +27,17 @@ CREATE TABLE species (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100)
   );
+
+  /*
+  Modify animals table:
+  Make sure that id is set as autoincremented PRIMARY KEY
+  Remove column species
+  Add column species_id which is a foreign key referencing species table
+  Add column owner_id which is a foreign key referencing the owners table
+  */
+
+  ALTER TABLE animals DROP COLUMN IF EXISTS species;
+  ALTER TABLE animals ADD COLUMN species_id INTEGER;
+  ALTER TABLE animals ADD CONSTRAINT fk_animals_species_id FOREIGN KEY (species_id) REFERENCES species (id);
+  ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+  ALTER TABLE animals ADD CONSTRAINT fk_animals_owner_id FOREIGN KEY (owner_id) REFERENCES owners (id);
